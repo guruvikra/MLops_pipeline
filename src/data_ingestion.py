c60bad6d12e4f5e428ccf92ac7392c969678a573
Guru Vikram <guru@atatus.com>
2025-03-27 23:57:33 +0530

    created first pipeline

---
+++ src/data_ingestion.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import os
+from sklearn.model_selection import train_test_split
+import logging
+
+# here is the location of the log files
+# Ensure logs directory is outside the "src" folder
+project_root = os.getcwd()
+logs_dir = os.path.join(project_root, "../logs")
+os.makedirs(logs_dir, exist_ok=True)
+
+# createing a object for logging
+logger = logging.getLogger('data_ingestion')
+logger.setLevel(logging.DEBUG)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+
+log_file_path = os.path.join(logs_dir, 'data_ingestion.log')
+file_handler = logging.FileHandler(log_file_path)
+file_handler.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+
+
+def load_data(file_url: str) -> pd.DataFrame:
+    """Load data from the csv file."""
+    try:
+        df = pd.read_csv(file_url)
+        logger.debug("data loaded from %s" , file_url)
+        return df
+    except Exception as e:
+        logger.error("%s", e)
+        raise
+
+def preprocess_data(df: pd.DataFrame)-> pd.DataFrame:
+    """Preprocess the data."""
+    try:
+        df.drop(columns=['Unnamed: 2','Unnamed: 3','Unnamed: 4'], inplace = True)
+        df.rename(columns= {'v1': 'target' , 'v2': 'text'}, inplace = True)
+        logger.debug('preprocessing completed')
+        return df
+    except Exception as e:
+        logger.error("%s", e)
+        raise
+
+def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame , url: str) -> None:
+    """Save the preprocessed data to a csv file."""
+    try:
+        data_path = os.path.join(url, 'raw')
+        os.makedirs(data_path, exist_ok=True)
+        train_data.to_csv(os.path.join(data_path,'train.csv'), index=False)
+        test_data.to_csv(os.path.join(data_path,'test.csv'), index=False)
+        logger.debug('data saved to %s and %s' , url + '_train.csv', url + '_test.csv')
+        logger.debug('data saved to %s ', data_path)
+    except Exception as e:
+        logger.error("%s", e)
+        raise
+
+
+
+def main():
+    try:
+        test_size = 0.2
+        data_path = '/home/namlabs/Guru/DVC/Spam-detection/MLops_pipeline/experiments/spam.csv'
+        df = load_data(file_url=data_path)
+        df_preprocessed = preprocess_data(df=df)
+        train_data, test_data = train_test_split(df_preprocessed, test_size=test_size, random_state=42)
+        save_data(train_data=train_data, test_data=test_data, url ='../data')
+    except Exception as e:
+        logger.error("%s", e)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/pre_processing.py
@@ -0,0 +1,109 @@
+import pandas as pd
+import os
+import logging
+from sklearn.preprocessing import LabelEncoder
+import nltk
+from nltk.stem.porter import  PorterStemmer
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+nltk.download("punkt")
+from nltk.tokenize import WordPunctTokenizer
+import string
+
+logs_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../logs")
+logs_dir = os.path.abspath(logs_dir)  # Convert to absolute path
+os.makedirs(logs_dir, exist_ok=True)
+
+# createing a object for logging
+logger = logging.getLogger('data_ingestion')
+logger.setLevel(logging.DEBUG)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+
+log_file_path = os.path.join(logs_dir, 'data_pre_processing.log')
+file_handler = logging.FileHandler(log_file_path)
+file_handler.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+tokenizer = WordPunctTokenizer()
+
+
+
+def transform_text(text):
+    """
+    Transforms the input text by converting it to lowercase, tokenizing, removing stopwords and punctuation, and stemming.
+    """
+    ps = PorterStemmer()
+    # Convert to lowercase
+    text = text.lower()
+    # Tokenize the text
+    text = tokenizer.tokenize(text)
+    # Remove non-alphanumeric tokens
+    text = [word for word in text if word.isalnum()]
+    # Remove stopwords and punctuation
+    text = [word for word in text if word not in stopwords.words('english') and word not in string.punctuation]
+    # Stem the words
+    text = [ps.stem(word) for word in text]
+    # Join the tokens back into a single string
+    return " ".join(text)
+
+
+def preprocess_data(df, text_col='text',target_col="target"):
+    """
+    Preprocesses the text data in the given DataFrame by applying the transform_text function.
+    """
+    try:
+        # Log the start of preprocessing
+        logger.info("Starting data preprocessing...")
+        encoder = LabelEncoder()
+
+        df[target_col] =  encoder.fit_transform(df[target_col])
+        logger.debug("encoded")
+        # Log the end of preprocessing
+        logger.info("Data preprocessing completed.")
+
+        df = df.drop_duplicates(keep = 'first')
+
+        # df[text_col] = df[text_col].apply(transform_text)
+        df.loc[:,text_col] = df[text_col].apply(transform_text)
+        logger.debug("text tranformed")
+        logger.debug(df.head())
+        return df
+    except Exception as e:
+        # Log any errors during preprocessing
+        logger.error(f"An error occurred during preprocessing: {str(e)}")
+        raise e
+
+def main():
+    """"main function"""
+    try:
+        train_data = pd.read_csv('/home/namlabs/Guru/DVC/Spam-detection/MLops_pipeline/data/raw/train.csv')
+        test_data = pd.read_csv('/home/namlabs/Guru/DVC/Spam-detection/MLops_pipeline/data/raw/test.csv')
+        logger.debug("data loaded")
+
+        train_data_processed = preprocess_data(train_data)
+        test_data_processed = preprocess_data(test_data)
+        logger.debug("data preprocessed")
+
+        data_path = os.path.join('../data',"interim")
+        os.makedirs(data_path, exist_ok=True)
+
+        train_data_processed.to_csv(os.path.join(data_path, "train_preprocessed.csv"), index=False)
+        test_data_processed.to_csv(os.path.join(data_path, "test_preprocessed.csv"), index=False)
+
+        logger.info("Data saved to interim folder.")
+    except Exception as e:
+    # Log any errors during data saving
+        logger.error(f"An error occurred during data saving: {str(e)}")
+        raise e
+
+if __name__ == "__main__":
+    main()
